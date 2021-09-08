82044830a71da6318cfb01f2ac49f5ffc692b819
aledeltoro <alejandroencarnacion1@gmail.com>
2021-09-08 04:07:14 -0700

    Implement ContentView, Model and ViewModel for MenuPage

---
+++ XamStyledMenuPage/XamStyledMenuPage/Models/MenuOption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XamStyledMenuPage.Models
+{
+    public class MenuOption : INotifyPropertyChanged
+    {
+        public MenuOption(string title, string detail, string image)
+        {
+            Title = title;
+            Image = image;
+            Detail = detail;
+        }
+
+        public string Title { get; }
+        public string Image { get; }
+        public string Detail { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ XamStyledMenuPage/XamStyledMenuPage/ViewModels/MenuViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamStyledMenuPage.Models;
+
+namespace XamStyledMenuPage.ViewModels
+{
+    public class MenuViewModel : BaseViewModel
+    {
+        public MenuOption BarAndHotelsOption { get; }
+        public MenuOption FineDiningOption { get; }
+        public MenuOption CafesOption { get; }
+        public MenuOption NearbyOption { get; }
+        public MenuOption FastFoodsOption { get; }
+        public MenuOption FeaturedFoodsOption { get; }
+
+        public MenuViewModel()
+        {
+            BarAndHotelsOption = new MenuOption("Bar and Hotels", "42 Places", "outline_sports_bar_black_48.png");
+            FineDiningOption = new MenuOption("Fine Dining", "15 Places", "outline_restaurant_black_48.png");
+            CafesOption = new MenuOption("Cafes", "28 Places", "outline_place_black_48.png");
+            NearbyOption = new MenuOption("Nearby", "34 Places", "outline_place_black_48.png");
+            FastFoodsOption = new MenuOption("Fast Foods", "29 Places", "outline_fastfood_black_48.png");
+            FeaturedFoodsOption = new MenuOption("Featured Foods", "21 Places", "outline_local_pizza_black_48.png");
+        }
+    }
+}
